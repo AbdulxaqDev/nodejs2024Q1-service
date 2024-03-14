78598202d87ec3815a0758a98cc5c0211762ae96
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-03-14 15:24:59 +0300

    update: update id validator

--- src/utils/id-validator.util.ts
+++ src/utils/id-validator.util.ts
@@ -1,14 +1,29 @@
-import { tResponse } from 'src/common/user/entities/user.entity';
+import { DBs, Endpoints } from 'src/entities/common.entity';
 import { validate } from 'uuid';
+import { response } from './response.util';
+import { HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
 
-export function isValidId(id: string): tResponse {
-  const response = { statusCode: 200, message: '', error: null };
+export function validateId(id: string, endPoint: Endpoints, res: Response) {
   if (validate(id)) {
-    response.message = this.userService.findOne(id);
+    const item = DBs[endPoint].find((i) => i.id === id);
+    if (item) {
+      return item;
+    } else {
+      response(
+        HttpStatus.NOT_FOUND,
+        `${endPoint.toUpperCase} Not Found`,
+        'Not Found',
+        res,
+      );
+    }
   } else {
-    response.statusCode = 400;
-    response.message = 'Invalid User Id';
-    response.error = 'Bad Request';
+    response(
+      HttpStatus.BAD_REQUEST,
+      `Invalid ${endPoint.toUpperCase} ID`,
+      'Back Request',
+      res,
+    );
   }
-  return response;
+  return false;
 }
