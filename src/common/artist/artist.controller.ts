228ae62e2797e5e4fdee9aee166ef218d890d928
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-03-15 16:58:18 +0300

    feat: almost complete artists endpoint

--- src/common/artist/artist.controller.ts
+++ src/common/artist/artist.controller.ts
@@ -1,34 +1,111 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpStatus,
+  HttpCode,
+  Header,
+  Res,
+} from '@nestjs/common';
 import { ArtistService } from './artist.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
+import { validateId } from 'src/utils/id-validator.util';
+import { Response } from 'express';
+import { response } from 'src/utils/response.util';
+import { Endpoints } from 'src/entities/common.entity';
 
-@Controller('artist')
+@Controller(Endpoints.ARTIST)
 export class ArtistController {
   constructor(private readonly artistService: ArtistService) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Content-Type', 'application/json')
   create(@Body() createArtistDto: CreateArtistDto) {
-    return this.artistService.create(createArtistDto);
+    const createdArtist = this.artistService.create(createArtistDto);
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: createdArtist,
+      error: null,
+    };
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
+  @Header('Content-Type', 'application/json')
   findAll() {
-    return this.artistService.findAll();
+    const artists = this.artistService.findAll();
+    return {
+      statusCode: HttpStatus.OK,
+      message: artists,
+      error: null,
+    };
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.artistService.findOne(+id);
+  @Header('Content-Type', 'application/json')
+  findOne(@Param('id') id: string, @Res() res: Response) {
+    const isValidIdAndArtist = validateId(id, Endpoints.ARTIST, res);
+
+    if (isValidIdAndArtist) {
+      return response(HttpStatus.OK, isValidIdAndArtist, null, res);
+    }
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateArtistDto: UpdateArtistDto) {
-    return this.artistService.update(+id, updateArtistDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateArtistDto: UpdateArtistDto,
+    @Res() res: Response,
+  ) {
+    const isValidIdAndArtist = validateId(id, Endpoints.ARTIST, res);
+    if (isValidIdAndArtist) {
+      return this.artistService.update(id, updateArtistDto);
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.artistService.remove(+id);
+  remove(@Param('id') id: string, @Res() res: Response) {
+    const isValidIdAndArtist = validateId(id, Endpoints.ARTIST, res);
+
+    if (isValidIdAndArtist) {
+      this.artistService.remove(isValidIdAndArtist);
+      return response(HttpStatus.NO_CONTENT, 'User deleted', null, res);
+    }
   }
 }
+
+// @Controller('artist')
+// export class ArtistController {
+//   constructor(private readonly artistService: ArtistService) {}
+
+//   @Post()
+//   create(@Body() createArtistDto: CreateArtistDto) {
+//     return this.artistService.create(createArtistDto);
+//   }
+
+//   @Get()
+//   findAll() {
+//     return this.artistService.findAll();
+//   }
+
+//   @Get(':id')
+//   findOne(@Param('id') id: string) {
+//     return this.artistService.findOne(+id);
+//   }
+
+//   @Patch(':id')
+//   update(@Param('id') id: string, @Body() updateArtistDto: UpdateArtistDto) {
+//     return this.artistService.update(+id, updateArtistDto);
+//   }
+
+//   @Delete(':id')
+//   remove(@Param('id') id: string) {
+//     return this.artistService.remove(+id);
+//   }
+// }
--- src/common/artist/artist.service.ts
+++ src/common/artist/artist.service.ts
@@ -1,26 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
+import { DBs, Endpoints } from 'src/entities/common.entity';
+import { response } from 'src/utils/response.util';
+import { Response } from 'express';
 
 @Injectable()
 export class ArtistService {
   create(createArtistDto: CreateArtistDto) {
-    return 'This action adds a new artist';
+    const { name, grammy } = createArtistDto;
+    const newArtist = new CreateArtistDto(name, grammy);
+
+    DBs[Endpoints.ARTIST].push(newArtist);
+
+    return newArtist;
   }
 
   findAll() {
-    return `This action returns all artist`;
+    return DBs[Endpoints.ARTIST];
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} artist`;
   }
 
-  update(id: number, updateArtistDto: UpdateArtistDto) {
-    return `This action updates a #${id} artist`;
+  update(id: string, updateArtistDto: UpdateArtistDto) {
+    const artist = DBs[Endpoints.ARTIST].find((u) => u.id === id);
+    artist.name = updateArtistDto.name;
+    artist.grammy = updateArtistDto.grammy;
+    return artist;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} artist`;
   }
 }
