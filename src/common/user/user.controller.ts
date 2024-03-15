3ef56e4cdcd5b4063ff771e44b299fcd431bdd66
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-03-15 06:40:43 +0300

    refactor: optimize user endpoint

--- src/common/user/user.controller.ts
+++ src/common/user/user.controller.ts
@@ -12,14 +12,14 @@ import {
   Res,
 } from '@nestjs/common';
 
-import { validate } from 'uuid';
 import { Response } from 'express';
 
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-user-password.dto';
 import { response } from 'src/utils/response.util';
-import { UsersDB } from 'src/db/db';
+import { validateId } from 'src/utils/id-validator.util';
+import { Endpoints } from 'src/entities/common.entity';
 
 @Controller('user')
 export class UserController {
@@ -47,27 +47,12 @@ export class UserController {
   @Get(':id')
   @Header('Content-Type', 'application/json')
   findOne(@Param('id') id: string, @Res() res: Response) {
-    if (validate(id)) {
-      const user = UsersDB.find((u) => u.id === id);
-      if (user) {
-        const { password, ...userWithoutPassword } = user;
-        return response(HttpStatus.OK, userWithoutPassword, null, res);
-      } else {
-        return response(
-          HttpStatus.NOT_FOUND,
-          'User Not Found',
-          'Not Found',
-          res,
-        );
-      }
-    }
+    const isValidIdAndUser = validateId(id, Endpoints.USER, res);
 
-    return response(
-      HttpStatus.BAD_REQUEST,
-      'Invalid User ID',
-      'Back Request',
-      res,
-    );
+    if (isValidIdAndUser) {
+      const { password, ...userWithoutPassword } = isValidIdAndUser;
+      return response(HttpStatus.OK, userWithoutPassword, null, res);
+    }
   }
 
   @Patch(':id')
@@ -76,60 +61,30 @@ export class UserController {
     @Body() updatePasswordDto: UpdatePasswordDto,
     @Res() res: Response,
   ) {
-    if (validate(id)) {
-      const user = UsersDB.find((_) => _.id === id);
+    const isValidIdAndUser = validateId(id, Endpoints.USER, res);
 
-      if (user) {
-        if (user.password === updatePasswordDto.oldPassword) {
-          const updatedUser = this.userService.update(id, updatePasswordDto);
-          return response(HttpStatus.OK, updatedUser, null, res);
-        } else if (user.password !== updatePasswordDto.oldPassword) {
-          return response(
-            HttpStatus.FORBIDDEN,
-            'Wrong Password',
-            'Forbidden',
-            res,
-          );
-        }
-      } else {
+    if (isValidIdAndUser) {
+      if (isValidIdAndUser.password === updatePasswordDto.oldPassword) {
+        const updatedUser = this.userService.update(id, updatePasswordDto);
+        return response(HttpStatus.OK, updatedUser, null, res);
+      } else if (isValidIdAndUser.password !== updatePasswordDto.oldPassword) {
         return response(
-          HttpStatus.NOT_FOUND,
-          'User Not Found',
-          'Not Found',
+          HttpStatus.FORBIDDEN,
+          'Wrong Password',
+          'Forbidden',
           res,
         );
       }
     }
-
-    return response(
-      HttpStatus.BAD_REQUEST,
-      'Invalid User ID',
-      'Bad Request',
-      res,
-    );
   }
 
   @Delete(':id')
   remove(@Param('id') id: string, @Res() res: Response) {
-    if (validate(id)) {
-      const user = UsersDB.find((u) => u.id === id);
-      if (user) {
-        this.userService.remove(user);
-        return response(HttpStatus.NO_CONTENT, 'User deleted', null, res);
-      } else {
-        return response(
-          HttpStatus.NOT_FOUND,
-          'User Not Found',
-          'Not Found',
-          res,
-        );
-      }
+    const isValidIdAndUser = validateId(id, Endpoints.USER, res);
+
+    if (isValidIdAndUser) {
+      this.userService.remove(isValidIdAndUser);
+      return response(HttpStatus.NO_CONTENT, 'User deleted', null, res);
     }
-    return response(
-      HttpStatus.BAD_REQUEST,
-      'Invalid User ID',
-      'Back Request',
-      res,
-    );
   }
 }
