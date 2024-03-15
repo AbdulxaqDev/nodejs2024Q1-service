c74904ac350f581b720c7f5f17b637dd3ecc8492
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-03-15 09:52:30 +0300

    feat: add favs endpoint create and update dtos

--- src/common/artist/dto/create-artist.dto.ts
+++ src/common/artist/dto/create-artist.dto.ts
@@ -1,1 +1,20 @@
-export class CreateArtistDto {}
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+import { v4 as uuidv4 } from 'uuid';
+
+export class CreateArtistDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  grammy: boolean;
+
+  id: string;
+
+  constructor(name: string, grammy: boolean) {
+    this.name = name;
+    this.grammy = grammy;
+    this.id = uuidv4(); // Generate UUID for id
+  }
+}
