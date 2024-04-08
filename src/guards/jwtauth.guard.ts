c9ff22e9672c251d38c355f32e2527550b737175
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-04-08 20:48:18 +0300

    fix: exclude some endpoints from jwt guard

--- src/guards/jwtauth.guard.ts
+++ src/guards/jwtauth.guard.ts
@@ -12,7 +12,15 @@ export class JwtAuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   canActivate(context: ExecutionContext): boolean {
+    const excludedRoutes = ['/auth/signup', '/auth/login', '/doc', '/'];
+
     const request = context.switchToHttp().getRequest();
+
+    const requestedRoute = request.path;
+    if (excludedRoutes.includes(requestedRoute)) {
+      return true;
+    }
+
     const authHeader = request.headers['authorization'];
 
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
